6fe935f7eb37ca75ff44dc2903985f48a0398378
marcelo goncalves <tchelojg@gmail.com>
2018-06-18 16:53:15 -0300

    Verificacao de login de usuário sendo feita.Tela de login sendo acionadaem caso negativo.

--- app/src/main/java/com/example/marcelo/instagramclone/home/HomeActivity.java
+++ app/src/main/java/com/example/marcelo/instagramclone/home/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.marcelo.instagramclone.home;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +10,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.example.marcelo.instagramclone.R;
 import com.example.marcelo.instagramclone.Utils.BottomNavigationViewAdapter;
 import com.example.marcelo.instagramclone.Utils.SectionsPagerAdapter;
 import com.example.marcelo.instagramclone.Utils.UniversalImageLoader;
+import com.example.marcelo.instagramclone.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -23,13 +27,17 @@ public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity";
     private Context mContext = HomeActivity.this;
     private final static int ACTIVITY_NUM = 0;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setupFirebaseAuth();
         initImageLoader();
         setupBottomNavigationView();
         setupViewPager();
+
 
     }
 
@@ -68,4 +76,53 @@ public class HomeActivity extends AppCompatActivity {
         UniversalImageLoader universalImageLoader = new  UniversalImageLoader(mContext);
         ImageLoader.getInstance().init(universalImageLoader.getConfig());
     }
+
+    /**
+     * Checks to see if the @param user is logged in
+     */
+    private void checkCurrentUser(FirebaseUser user){
+        Log.d(TAG, "checkCurrentUser: Checking if the user is logged in!");
+        if (user == null){
+            
+            Intent intent = new Intent(mContext, LoginActivity.class);
+            startActivity(intent);
+                    
+        }
+    }
+
+   // ************************************* FIREBASE *************************************************
+
+    private void setupFirebaseAuth(){
+
+        Log.d(TAG, "setupFirebaseAuth: Setting up firebase.auth!");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                checkCurrentUser(user);
+                if (user != null){
+                    Log.d(TAG, "onAuthStateChanged: user sign_in" + user.getUid());
+
+                }else {
+                    Log.d(TAG, "onAuthStateChanged: user sign_out");
+                }
+            }
+        };
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if (mAuthStateListener != null){
+            mAuth.addAuthStateListener(mAuthStateListener);
+        }
+    }
+
 }
