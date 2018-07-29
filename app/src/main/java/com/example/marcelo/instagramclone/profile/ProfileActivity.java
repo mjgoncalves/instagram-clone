19f168379f629affe52afedcf80f0dcc037c5c1b
marcelo goncalves <tchelojg@gmail.com>
2018-07-29 16:28:57 -0300

    improving profile activity and fragment

--- app/src/main/java/com/example/marcelo/instagramclone/profile/ProfileActivity.java
+++ app/src/main/java/com/example/marcelo/instagramclone/profile/ProfileActivity.java
@@ -1,5 +1,8 @@
 package com.example.marcelo.instagramclone.profile;
 
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -37,88 +40,98 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         Log.d(TAG, "onCreate: started");
-        setupBottomNavigationView();
-        setupToolbar();
-        setupActivityWidgets();
-        setProfileImage();
-        tempGridSetup();
+        init();
+//        setupBottomNavigationView();
+//        setupToolbar();
+//        setupActivityWidgets();
+//        setProfileImage();
+//        tempGridSetup();
 
     }
-
-    private void setupToolbar(){
-
-        Toolbar toolbar = findViewById(R.id.profileToolBar);
-        setSupportActionBar(toolbar);
-        ImageView profileMenu = findViewById(R.id.profile_menu_settings);
-        profileMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: Navigating to account settings");
-                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
-                startActivity(intent);
-            }
-        });
+    
+    private void init(){
+        Log.d(TAG, "init: Inflating " + getString(R.string.fragment_profile));
+        ProfileFragment profileFragment = new ProfileFragment();
+        FragmentTransaction transaction = ProfileActivity.this.getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, profileFragment);
+        transaction.addToBackStack(getString(R.string.fragment_profile));
+        transaction.commit();
     }
 
-    private void setProfileImage(){
-        Log.d(TAG, "setProfileImage: Setting up profile photo!");
-        String imgUrl = "https://http2.mlstatic.com/ursinho-de-pelucia-aviador-25cm-decoraco-anti-alergico-D_NQ_NP_830415-MLB25232632902_122016-F.jpg";
-        UniversalImageLoader.setImage(imgUrl, profilePhoto, progressBar, "");
-
-
-    }
-
-    private void setupActivityWidgets(){
-
-        Log.d(TAG, "setupActivityWidgets: Setting up profileActivity widgets!");
-        progressBar = findViewById(R.id.progress_bar);
-        progressBar.setVisibility(View.GONE);
-        profilePhoto = findViewById(R.id.profile_photo);
-        gridView = findViewById(R.id.gridview);
-
-
-    }
-
-    private void setupImageGrid(ArrayList<String> imgURLs){
-        GridImageAdapter adapter = new GridImageAdapter(mContext, R.layout.layout_grid_imageview, "", imgURLs);
-        gridView.setAdapter(adapter);
-
-        int gridWidth = getResources().getDisplayMetrics().widthPixels;
-        final int GRID_COL_NUM = 3;
-        int imageWidth = gridWidth/GRID_COL_NUM;
-        gridView.setColumnWidth(imageWidth);
-
-    }
-
-    private void tempGridSetup(){
-        ArrayList<String> imgURLs = new ArrayList<>();
-        imgURLs.add("http://1.bp.blogspot.com/-gB55QpVTLW4/VpjLCZOT5FI/AAAAAAAAk8s/kpOjWY5_ec0/s1600/IMG_3075.JPG");
-        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
-        imgURLs.add("https://abrilviagemeturismo.files.wordpress.com/2016/12/11-120241176.jpg?quality=70&strip=info&w=680&h=453&crop=1");
-        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
-        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
-        imgURLs.add("http://travelzom.com/wp-content/uploads/2017/06/bali-indonesia-2.jpg");
-        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
-        imgURLs.add("https://abrilviagemeturismo.files.wordpress.com/2016/12/11-120241176.jpg?quality=70&strip=info&w=680&h=453&crop=1");
-        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
-        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
-        imgURLs.add("http://1.bp.blogspot.com/-gB55QpVTLW4/VpjLCZOT5FI/AAAAAAAAk8s/kpOjWY5_ec0/s1600/IMG_3075.JPG");
-        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
-        imgURLs.add("https://abrilviagemeturismo.files.wordpress.com/2016/12/11-120241176.jpg?quality=70&strip=info&w=680&h=453&crop=1");
-        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
-        setupImageGrid(imgURLs);
-
-    }
-
-    public void setupBottomNavigationView(){
-
-        Log.d(TAG, "setupBottomNavigationView: Starting BottonNavigationViewEx");
-        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.botton_navView);
-        BottomNavigationViewAdapter.adaptBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewAdapter.enableNavigations(mContext, bottomNavigationViewEx);
-        Menu menu = bottomNavigationViewEx.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setChecked(true);
-    }
+//    private void setupToolbar(){
+//
+//        Toolbar toolbar = findViewById(R.id.profileToolBar);
+//        setSupportActionBar(toolbar);
+//        ImageView profileMenu = findViewById(R.id.profile_menu_settings);
+//        profileMenu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: Navigating to account settings");
+//                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//
+//    private void setProfileImage(){
+//        Log.d(TAG, "setProfileImage: Setting up profile photo!");
+//        String imgUrl = "https://http2.mlstatic.com/ursinho-de-pelucia-aviador-25cm-decoraco-anti-alergico-D_NQ_NP_830415-MLB25232632902_122016-F.jpg";
+//        UniversalImageLoader.setImage(imgUrl, profilePhoto, progressBar, "");
+//
+//
+//    }
+//
+//    private void setupActivityWidgets(){
+//
+//        Log.d(TAG, "setupActivityWidgets: Setting up profileActivity widgets!");
+//        progressBar = findViewById(R.id.progress_bar);
+//        progressBar.setVisibility(View.GONE);
+//        profilePhoto = findViewById(R.id.profile_photo);
+//        gridView = findViewById(R.id.gridview);
+//
+//
+//    }
+//
+//    private void setupImageGrid(ArrayList<String> imgURLs){
+//        GridImageAdapter adapter = new GridImageAdapter(mContext, R.layout.layout_grid_imageview, "", imgURLs);
+//        gridView.setAdapter(adapter);
+//
+//        int gridWidth = getResources().getDisplayMetrics().widthPixels;
+//        final int GRID_COL_NUM = 3;
+//        int imageWidth = gridWidth/GRID_COL_NUM;
+//        gridView.setColumnWidth(imageWidth);
+//
+//    }
+//
+//    private void tempGridSetup(){
+//        ArrayList<String> imgURLs = new ArrayList<>();
+//        imgURLs.add("http://1.bp.blogspot.com/-gB55QpVTLW4/VpjLCZOT5FI/AAAAAAAAk8s/kpOjWY5_ec0/s1600/IMG_3075.JPG");
+//        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
+//        imgURLs.add("https://abrilviagemeturismo.files.wordpress.com/2016/12/11-120241176.jpg?quality=70&strip=info&w=680&h=453&crop=1");
+//        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
+//        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
+//        imgURLs.add("http://travelzom.com/wp-content/uploads/2017/06/bali-indonesia-2.jpg");
+//        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
+//        imgURLs.add("https://abrilviagemeturismo.files.wordpress.com/2016/12/11-120241176.jpg?quality=70&strip=info&w=680&h=453&crop=1");
+//        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
+//        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
+//        imgURLs.add("http://1.bp.blogspot.com/-gB55QpVTLW4/VpjLCZOT5FI/AAAAAAAAk8s/kpOjWY5_ec0/s1600/IMG_3075.JPG");
+//        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
+//        imgURLs.add("https://abrilviagemeturismo.files.wordpress.com/2016/12/11-120241176.jpg?quality=70&strip=info&w=680&h=453&crop=1");
+//        imgURLs.add("https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2017/09/Otagi-Nenbutsu-ji-Shrine.jpg");
+//        setupImageGrid(imgURLs);
+//
+//    }
+//
+//    public void setupBottomNavigationView(){
+//
+//        Log.d(TAG, "setupBottomNavigationView: Starting BottonNavigationViewEx");
+//        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.botton_navView);
+//        BottomNavigationViewAdapter.adaptBottomNavigationView(bottomNavigationViewEx);
+//        BottomNavigationViewAdapter.enableNavigations(mContext, bottomNavigationViewEx);
+//        Menu menu = bottomNavigationViewEx.getMenu();
+//        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+//        menuItem.setChecked(true);
+//    }
 
 }
---
+++ app/src/main/java/com/example/marcelo/instagramclone/profile/ProfileFragment.java
@@ -0,0 +1,21 @@
+package com.example.marcelo.instagramclone.profile;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.marcelo.instagramclone.R;
+
+public class ProfileFragment extends Fragment {
+    private static final String TAG = "ProfileFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
+    }
+}
