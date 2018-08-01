259f99969b64116bf3a8105bb1a598f82a6f4cfa
marcelo goncalves <tchelojg@gmail.com>
2018-08-01 17:54:44 -0300

    setting users information retrieving from database

---
+++ app/src/main/java/com/example/marcelo/instagramclone/Models/UsersSettings.java
@@ -0,0 +1,42 @@
+package com.example.marcelo.instagramclone.Utils;
+
+import com.example.marcelo.instagramclone.Models.UserAccountSettings;
+import com.example.marcelo.instagramclone.Models.Users;
+
+public class UserSettings {
+    private Users users;
+    private UserAccountSettings settings;
+
+    public UserSettings(Users users, UserAccountSettings settings) {
+        this.users = users;
+        this.settings = settings;
+    }
+
+    public UserSettings() {
+
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+    public UserAccountSettings getSettings() {
+        return settings;
+    }
+
+    public void setSettings(UserAccountSettings settings) {
+        this.settings = settings;
+    }
+
+    @Override
+    public String toString() {
+        return "UserSettings{" +
+                "users=" + users +
+                ", settings=" + settings +
+                '}';
+    }
+}
